15d7de542f4705bdc96dc3920af8375e8545a56e
yahel manor <yahelmanor20@gmail.com>
2020-07-24 10:15:49 +0300

    added animation

--- init.js
+++ init.js
@@ -134,150 +134,152 @@ function sleep(milliseconds) {
   /*
   calculate the distance between 2 points
   */
-  function distance(A,B)
-  {
-      return Math.floor(Math.sqrt((Math.pow(A.x-B.x,2)+Math.pow(A.y-B.y,2))))*10;
-  }
-  /*
-  calculate the distance between the start point and another point
-  */
-  function calculateGcost(P)
-  {
-      return distance(P,start)
-  }
-  /*
-  calculate the distance between the end point and another point
-  */
-  function calculateHcost(P)
-  {
-      return distance(P,end)
-  }
-  /*
-  go through a node and search for a givven point
-  */
-  function haventBeenThere(x,y,ExpendedNode)
-  {
-      let curr = ExpendedNode;
-      while (curr)
-      {
-          if(curr.point.x == x && curr.point.y == y)
-              return false;
-          curr = curr.node;
-      }
-  
-      return true;
-  }
-  /*
-  expanding searching area
-  return an array of nodes
-  */
-  function ExpandArray(ExpendedNode)
-  {
-      let NodesArray = [];
-      let width = squars.length;
-      let height = squars[0].length;
-      let x = ExpendedNode.point.x;
-      let y = ExpendedNode.point.y;
-      //check if posible to go there and then create and push a new node
-      if(x-1 >= 0 && squars[x-1][y] != "black" && haventBeenThere(x-1,y,ExpendedNode))
-          NodesArray.push(new Node(new Point(x-1,y,"green"),ExpendedNode));
-      if(x+1 < width && squars[x+1][y] != "black" && haventBeenThere(x+1,y,ExpendedNode))
-          NodesArray.push(new Node(new Point(x+1,y,"green"),ExpendedNode));
-      if(y-1 >= 0 && squars[x][y-1] != "black" && haventBeenThere(x,y-1,ExpendedNode))
-          NodesArray.push(new Node(new Point(x,y-1,"green"),ExpendedNode));
-      if(y+1 < height && squars[x][y+1] != "black" && haventBeenThere(x,y+1,ExpendedNode))
-          NodesArray.push(new Node(new Point(x,y+1,"green"),ExpendedNode));
-      for (let index = 0; index < NodesArray.length; index++) 
-          NodesArray[index].upgrade();
-          
-      return NodesArray;
-  }
-  /*
-  sorting the nodes by their F cost
-  then by their H cost
-  */
-  function sortByFcost(nodeArray)
-  {
-      for (let X = 0; X < nodeArray.length; X++) {
-          for (let Y = 0; Y < nodeArray.length - 1; Y++) {
-              if(nodeArray[Y].point.F_cost > nodeArray[Y + 1].point.F_cost)
-              {
-                  let temp = nodeArray[Y + 1];
-                  nodeArray[Y + 1] = nodeArray[Y];
-                  nodeArray[Y] = temp;
-              }
-              else if((nodeArray[Y].point.F_cost == nodeArray[Y + 1].point.F_cost) && (nodeArray[Y].point.H_cost < nodeArray[Y + 1].point.H_cost))
-              {
-                  let temp = nodeArray[Y + 1];
-                  nodeArray[Y + 1] = nodeArray[Y];
-                  nodeArray[Y] = temp;
-              }
-          }
-      }
-      let f = nodeArray[0].point.F_cost;
-      for (let X = 0; X < nodeArray.length; X++) {
-          for (let Y = 0; Y < nodeArray.length - 1; Y++) {
-              if((nodeArray[Y].point.F_cost == f) && (nodeArray[Y].point.H_cost > nodeArray[Y + 1].point.H_cost))
-              {
-                  let temp = nodeArray[Y + 1];
-                  nodeArray[Y + 1] = nodeArray[Y];
-                  nodeArray[Y] = temp;
-              }
-              
-          }
-      }
-      return nodeArray;
-  }
-  /*
-  main function of the algorithm
-  using linked list
-  */
-  function A_algorithm()
-  {
-      let firsts = [];
-      let startNode = new Node(start,null);
-      let NodesArray = ExpandArray(startNode);//initiate the nodews array with the start point
-      // startNode => start || NodesArray => [up, down, left, right]
-      let bestTrace = null; 
-      var exit = true;
-      NodesArray.forEach(n => {//checking if the start is a neighbore of the end
-              if(n.point.x == end.x && n.point.y == end.y){
-                  bestTrace = n; 
-                  exit = false;
-              }
-      });
-      while(exit)
-      {
-          if(NodesArray.length == 0){
-              exit = false;
-              squars[end.x][end.y] = "yellow"
-              squars[start.x][start.y] = "yellow"
-              alert("no path avalible!");
-              continue;
-          }
-          NodesArray = sortByFcost(NodesArray);//searching for the closest to the end
-          tempNodeArray = ExpandArray(NodesArray[0]);
-          firsts.push(NodesArray.splice(0,1)); // array: [1,2,3,4] splice(0,1) : [1]
-          NodesArray.push.apply(NodesArray,tempNodeArray);//adding next gen
-          var i = 0;
-          NodesArray.forEach(n => {//making progress, sign it and check if finish
-              if(n.point.x == end.x && n.point.y == end.y){
-                  bestTrace = n; 
-                  exit = false;
-              }
-              firsts.forEach(first => {//checking for false run - removing create a minor bug
-                  if(first[0].point.x == n.point.x && first[0].point.y == n.point.y){
-                      NodesArray.splice(i,1);
-                  }
-              });
-              squars[n.point.x][n.point.y] = "#123456";//mark the way:)
-              i++;
-          });
-          printSquares();
-      }
-      return bestTrace;
-  }
-  function draw_path(e) {
+function distance(A,B)
+{
+    return Math.floor(Math.sqrt((Math.pow(A.x-B.x,2)+Math.pow(A.y-B.y,2))))*10;
+}
+/*
+calculate the distance between the start point and another point
+*/
+function calculateGcost(P)
+{
+    return distance(P,start)
+}
+/*
+calculate the distance between the end point and another point
+*/
+function calculateHcost(P)
+{
+    return distance(P,end)
+}
+/*
+go through a node and search for a givven point
+*/
+function haventBeenThere(x,y,ExpendedNode)
+{
+    let curr = ExpendedNode;
+    while (curr)
+    {
+        if(curr.point.x == x && curr.point.y == y)
+            return false;
+        curr = curr.node;
+    }
+
+    return true;
+}
+/*
+expanding searching area
+return an array of nodes
+*/
+function ExpandArray(ExpendedNode)
+{
+    let NodesArray = [];
+    let width = squars.length;
+    let height = squars[0].length;
+    let x = ExpendedNode.point.x;
+    let y = ExpendedNode.point.y;
+    //check if posible to go there and then create and push a new node
+    if(x-1 >= 0 && squars[x-1][y] != "black" && haventBeenThere(x-1,y,ExpendedNode))
+        NodesArray.push(new Node(new Point(x-1,y,"green"),ExpendedNode));
+    if(x+1 < width && squars[x+1][y] != "black" && haventBeenThere(x+1,y,ExpendedNode))
+        NodesArray.push(new Node(new Point(x+1,y,"green"),ExpendedNode));
+    if(y-1 >= 0 && squars[x][y-1] != "black" && haventBeenThere(x,y-1,ExpendedNode))
+        NodesArray.push(new Node(new Point(x,y-1,"green"),ExpendedNode));
+    if(y+1 < height && squars[x][y+1] != "black" && haventBeenThere(x,y+1,ExpendedNode))
+        NodesArray.push(new Node(new Point(x,y+1,"green"),ExpendedNode));
+    for (let index = 0; index < NodesArray.length; index++) 
+        NodesArray[index].upgrade();
+        
+    return NodesArray;
+}
+/*
+sorting the nodes by their F cost
+then by their H cost
+*/
+function sortByFcost(nodeArray)
+{
+    for (let X = 0; X < nodeArray.length; X++) {
+        for (let Y = 0; Y < nodeArray.length - 1; Y++) {
+            if(nodeArray[Y].point.F_cost > nodeArray[Y + 1].point.F_cost)
+            {
+                let temp = nodeArray[Y + 1];
+                nodeArray[Y + 1] = nodeArray[Y];
+                nodeArray[Y] = temp;
+            }
+            else if((nodeArray[Y].point.F_cost == nodeArray[Y + 1].point.F_cost) && (nodeArray[Y].point.H_cost < nodeArray[Y + 1].point.H_cost))
+            {
+                let temp = nodeArray[Y + 1];
+                nodeArray[Y + 1] = nodeArray[Y];
+                nodeArray[Y] = temp;
+            }
+        }
+    }
+    let f = nodeArray[0].point.F_cost;
+    for (let X = 0; X < nodeArray.length; X++) {
+        for (let Y = 0; Y < nodeArray.length - 1; Y++) {
+            if((nodeArray[Y].point.F_cost == f) && (nodeArray[Y].point.H_cost > nodeArray[Y + 1].point.H_cost))
+            {
+                let temp = nodeArray[Y + 1];
+                nodeArray[Y + 1] = nodeArray[Y];
+                nodeArray[Y] = temp;
+            }
+            
+        }
+    }
+    return nodeArray;
+}
+/*
+main function of the algorithm
+using linked list
+*/
+function A_algorithm()
+{
+    let firsts = [];
+    let startNode = new Node(start,null);
+    let NodesArray = ExpandArray(startNode);//initiate the nodews array with the start point
+    // startNode => start || NodesArray => [up, down, left, right]
+    let bestTrace = null; 
+    var exit = true;
+    NodesArray.forEach(n => {//checking if the start is a neighbore of the end
+            if(n.point.x == end.x && n.point.y == end.y){
+                bestTrace = n; 
+                exit = false;
+            }
+    });
+    while(exit)
+    {
+        if(NodesArray.length == 0){
+            exit = false;
+            squars[end.x][end.y] = "yellow"
+            squars[start.x][start.y] = "yellow"
+            alert("no path avalible!");
+            continue;
+        }
+        NodesArray = sortByFcost(NodesArray);//searching for the closest to the end
+        tempNodeArray = ExpandArray(NodesArray[0]);
+        firsts.push(NodesArray.splice(0,1)); // array: [1,2,3,4] splice(0,1) : [1]
+        NodesArray.push.apply(NodesArray,tempNodeArray);//adding next gen
+        var i = 0;
+        NodesArray.forEach(n => {//making progress, sign it and check if finish
+            if(n.point.x == end.x && n.point.y == end.y){
+                bestTrace = n; 
+                exit = false;
+            }
+            firsts.forEach(first => {//checking for false run - removing create a minor bug
+                if(first[0].point.x == n.point.x && first[0].point.y == n.point.y){
+                    NodesArray.splice(i,1);
+                }
+            });
+            squars[n.point.x][n.point.y] = "#123456";//mark the way:)
+            i++;
+        });
+        printSquares();
+    }
+    return bestTrace;
+}
+
+path_result = Object;
+function draw_path(e) {
     let result = A_algorithm();
     if(result == null){
         printSquares();
@@ -286,11 +288,23 @@ function sleep(milliseconds) {
     result = result.node;
     squars[end.x][end.y] = "red";
     squars[start.x][start.y] = "blue";
+    path_result = result;
+    setInterval(draw_animation, 1000/15);
+    // path_result = result;
+    // while(result.node)
+    // {
+    //     squars[result.point.x][result.point.y] = "#00ffcc";  
+    //     result = result.node;
+    // }
+    //printSquares();
+}
 
-    while(result.node)
-    {
-        squars[result.point.x][result.point.y] = "#00ffcc";  
-        result = result.node;
+function draw_animation() {
+    if (path_result.node == null) {
+        return false;
     }
+    
+    squars[path_result.point.x][path_result.point.y] = "#00ffcc";  
+    path_result = path_result.node;
     printSquares();
 }
